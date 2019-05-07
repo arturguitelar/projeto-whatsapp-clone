0c746f684af027d322dd2305a2d010001c0ed277
Artur Killdragon <artur.guitelar@gmail.com>
2019-05-07 08:55:30 -0300

    Add: Método para tirar fotografia. Já esconde/mostra os botões e telas necessárias no processo.

--- app/src/controllers/CameraController.js
+++ app/src/controllers/CameraController.js
@@ -16,11 +16,32 @@ export class CameraController {
     }
 
     /**
-     * Pára a câmera.
+     * Pára todos os tracks da câmera.
      */
     stop() {
         this._stream.getTracks().forEach(track => {
             track.stop();
         });
+    }
+
+    /**
+     * - Cria um elemento canvas com altura e largura do vídeo.
+     * - Desenha no contexto do canvas a partir da imagem da câmera.
+     * 
+     * @param {String} mimeType Tipo de imagem. Default: image/png.
+     * @returns {String} Imagem em base64.
+     */
+    takePicture(mimeType = 'image/png') {
+
+        let canvas = document.createElement('canvas');
+
+        canvas.setAttribute('width', this._videoEl.videoWidth);
+        canvas.setAttribute('height', this._videoEl.videoHeight);
+
+        let context = canvas.getContext('2d');
+
+        context.drawImage(this._videoEl, 0, 0, canvas.width, canvas.height);
+
+        return canvas.toDataURL(mimeType);
     }
 }
--- app/src/controllers/WhatsAppController.js
+++ app/src/controllers/WhatsAppController.js
@@ -151,7 +151,29 @@ export class WhatsAppController {
         });
 
         this.el.btnTakePicture.on('click', e => {
-            console.log('take picture');
+            
+            let dataURL = this._camera.takePicture();
+
+            this.el.pictureCamera.src = dataURL;
+            
+            this.el.pictureCamera.show();
+            this.el.videoCamera.hide();
+            this.el.btnReshootPanelCamera.show();
+            this.el.containerTakePicture.hide();
+            this.el.containerSendPicture.show();
+        });
+
+        this.el.btnReshootPanelCamera.on('click', e => {
+
+            this.el.pictureCamera.hide();
+            this.el.videoCamera.show();
+            this.el.btnReshootPanelCamera.hide();
+            this.el.containerTakePicture.show();
+            this.el.containerSendPicture.hide();
+        });
+
+        this.el.btnSendPicture.on('click', e => {
+            console.log(this.el.pictureCamera.src);
         });
 
         this.el.btnAttachDocument.on('click', e => {
