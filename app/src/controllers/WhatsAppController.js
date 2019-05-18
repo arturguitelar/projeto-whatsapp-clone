6e17331acc9fa21b60cd127b57dc4f2bb4fd6531
Artur Killdragon <artur.guitelar@gmail.com>
2019-05-18 00:27:25 -0300

    Add: Evento de click em um contato.

--- app/src/controllers/WhatsAppController.js
+++ app/src/controllers/WhatsAppController.js
@@ -144,6 +144,26 @@ export class WhatsAppController {
                     img.show();
                 }
 
+                // click para mostrar a tela de conversa deste contato
+                div.on('click', e => {
+
+                    this.el.activeName.innerHTML = contact.name;
+                    this.el.activeStatus.innerHTML = contact.status;
+
+                    if (contact.photo) {
+                        let img = this.el.activePhoto;
+
+                        img.src = contact.photo;
+                        img.show();
+                    }
+
+                    this.el.home.hide();
+
+                    this.el.main.css({
+                        display: 'flex'
+                    });
+                });
+
                 this.el.contactsMessagesList.appendChild(div);
             });
         });
